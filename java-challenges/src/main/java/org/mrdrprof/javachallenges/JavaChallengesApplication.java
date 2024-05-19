80be5ff7fcf5e8e4470438f59edf74b7a70b2916
Alex Golub <a12thu@gmail.com>
2024-05-19 23:38:53 +0300

    Init: solving real-world coding challenges + Java Challenges_ 100+ Proven Tasks

---
+++ java-challenges/src/main/java/org/mrdrprof/javachallenges/JavaChallengesApplication.java
@@ -0,0 +1,13 @@
+package org.mrdrprof.javachallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallengesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaChallengesApplication.class, args);
+    }
+
+}
---
+++ java-challenges/src/test/java/org/mrdrprof/javachallenges/JavaChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrdrprof.javachallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChallengesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
